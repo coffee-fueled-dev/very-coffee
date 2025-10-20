d95eb9c2bab55155eb982f1ec29b2601eccb748f
Zach Garrett <zach@very.coffee>
2025-10-19 22:17:16 -0400

    formatting change

--- packages/app/src/routes/blog.index.tsx
+++ packages/app/src/routes/blog.index.tsx
@@ -8,16 +8,14 @@ import { PostProvider } from "@/contexts/post-context";
 import { PostBreadcrumb } from "@/components/blocks/post-breadcrumb";
 
 export const Route = createFileRoute("/blog/")({
-  component: () => {
-    return (
-      <PostProvider splat={undefined}>
-        <section className="space-y-6">
-          <PostBreadcrumb />
-          <PostHeader />
-          <PostPageContent />
-          <PostPreviews sectionTitle="Topics" />
-        </section>
-      </PostProvider>
-    );
-  },
+  component: () => (
+    <PostProvider splat={undefined}>
+      <section className="space-y-6">
+        <PostBreadcrumb />
+        <PostHeader />
+        <PostPageContent />
+        <PostPreviews sectionTitle="Topics" />
+      </section>
+    </PostProvider>
+  ),
 });
